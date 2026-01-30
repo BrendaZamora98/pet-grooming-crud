7debbb26b6b91206af3cbd01d570f900e96a7d98
Brenda Zamora <257559484+BrendaZamora98@users.noreply.github.com>
2026-01-30 18:09:57 -0500

    Add getters and setters for Pet and Owner

--- src/main/java/com/mycompany/petgrooming/logic/Owner.java
+++ src/main/java/com/mycompany/petgrooming/logic/Owner.java
@@ -2,11 +2,14 @@ package com.mycompany.petgrooming.logic;
 
 public class Owner {
     
+    //Attributes
     private int ownerId;
     private String ownerName;
     private String phone;
     private String address;
 
+    
+    //Constructors
     public Owner() {
     }
 
@@ -14,6 +17,40 @@ public class Owner {
         this.ownerId = ownerId;
         this.ownerName = ownerName;
         this.phone = phone;
+        this.address = address;
+    }
+
+    
+    //Methods
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
         this.address = address;
     }
     
--- src/main/java/com/mycompany/petgrooming/logic/Pet.java
+++ src/main/java/com/mycompany/petgrooming/logic/Pet.java
@@ -2,23 +2,86 @@ package com.mycompany.petgrooming.logic;
 
 public class Pet {
     
+    //Attributes
     private int petId;
     private String petName;
     private String breed;
     private String color;
     private String allergies;
     private String specialCare;
-
+    private Owner theOwner;
+    
+    
+    //Contructors
     public Pet() {
     }
 
-    public Pet(int petId, String petName, String breed, String color, String allergies, String specialCare) {
+    public Pet(int petId, String petName, String breed, String color, String allergies, String specialCare, Owner theOwner) {
         this.petId = petId;
         this.petName = petName;
         this.breed = breed;
         this.color = color;
         this.allergies = allergies;
         this.specialCare = specialCare;
+        this.theOwner = theOwner;
+    }
+
+        
+    //Methods
+    public int getPetId() {
+        return petId;
+    }
+
+    public void setPetId(int petId) {
+        this.petId = petId;
+    }
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(String allergies) {
+        this.allergies = allergies;
+    }
+
+    public String getSpecialCare() {
+        return specialCare;
+    }
+
+    public void setSpecialCare(String specialCare) {
+        this.specialCare = specialCare;
+    }
+
+    public Owner getTheOwner() {
+        return theOwner;
+    }
+
+    public void setTheOwner(Owner theOwner) {
+        this.theOwner = theOwner;
     }
     
 }
