847b80cdc81d3a761a58a31314572f861bdc8e3f
Brenda Zamora <257559484+BrendaZamora98@users.noreply.github.com>
2026-01-30 17:35:04 -0500

    Add attributes and constructors to Pet and Owner classes

--- src/main/java/com/mycompany/petgrooming/logic/Owner.java
+++ src/main/java/com/mycompany/petgrooming/logic/Owner.java
@@ -2,4 +2,19 @@ package com.mycompany.petgrooming.logic;
 
 public class Owner {
     
+    private int ownerId;
+    private String ownerName;
+    private String phone;
+    private String address;
+
+    public Owner() {
+    }
+
+    public Owner(int ownerId, String ownerName, String phone, String address) {
+        this.ownerId = ownerId;
+        this.ownerName = ownerName;
+        this.phone = phone;
+        this.address = address;
+    }
+    
 }
--- src/main/java/com/mycompany/petgrooming/logic/Pet.java
+++ src/main/java/com/mycompany/petgrooming/logic/Pet.java
@@ -2,4 +2,23 @@ package com.mycompany.petgrooming.logic;
 
 public class Pet {
     
+    private int petId;
+    private String petName;
+    private String breed;
+    private String color;
+    private String allergies;
+    private String specialCare;
+
+    public Pet() {
+    }
+
+    public Pet(int petId, String petName, String breed, String color, String allergies, String specialCare) {
+        this.petId = petId;
+        this.petName = petName;
+        this.breed = breed;
+        this.color = color;
+        this.allergies = allergies;
+        this.specialCare = specialCare;
+    }
+    
 }
