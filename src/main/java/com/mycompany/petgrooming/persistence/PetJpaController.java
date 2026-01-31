ee7699e7830d218b25dd01783cd88b06f6bc4fe1
Brenda Zamora <257559484+BrendaZamora98@users.noreply.github.com>
2026-01-31 14:04:28 -0500

    Create the constructors for the JPA controllers

--- src/main/java/com/mycompany/petgrooming/persistence/PetJpaController.java
+++ src/main/java/com/mycompany/petgrooming/persistence/PetJpaController.java
@@ -7,6 +7,7 @@ import jakarta.persistence.EntityManagerFactory;
 import java.io.Serializable;
 import jakarta.persistence.Query;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.Persistence;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
 import java.util.List;
@@ -17,6 +18,10 @@ public class PetJpaController implements Serializable {
         this.emf = emf;
     }
     private EntityManagerFactory emf = null;
+    
+    public PetJpaController() {
+        emf = Persistence.createEntityManagerFactory("PetGroomingPU");
+    }
 
     public EntityManager getEntityManager() {
         return emf.createEntityManager();
