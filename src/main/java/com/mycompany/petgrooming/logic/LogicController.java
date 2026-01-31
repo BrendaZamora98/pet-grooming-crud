0a2a3c4061f61fe2a60055c0c1451d31c13ab00f
Brenda Zamora <257559484+BrendaZamora98@users.noreply.github.com>
2026-01-31 14:19:28 -0500

    Create instances of logic and persistence controllers

--- src/main/java/com/mycompany/petgrooming/logic/LogicController.java
+++ src/main/java/com/mycompany/petgrooming/logic/LogicController.java
@@ -1,5 +1,7 @@
 package com.mycompany.petgrooming.logic;
 
+import com.mycompany.petgrooming.persistence.PersistenceController;
+
 public class LogicController {
-    
+    PersistenceController persisControl= new PersistenceController();
 }
--- src/main/java/com/mycompany/petgrooming/persistence/PersistenceController.java
+++ src/main/java/com/mycompany/petgrooming/persistence/PersistenceController.java
@@ -2,4 +2,7 @@ package com.mycompany.petgrooming.persistence;
 
 public class PersistenceController {
     
+    PetJpaController petJpa= new PetJpaController();
+    OwnerJpaController ownerJpa= new OwnerJpaController();
+    
 }
